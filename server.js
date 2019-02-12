ad1455aa2a94108375c73170491987a640787386
Indika Weerakoon <wmhindika@gmail.com>
2019-02-12 14:53:50 +0530

    correction on deployment errors

--- server.js
+++ server.js
@@ -1,22 +1,16 @@
 //Install express server
 const express = require('express');
-const http = require('http')
 const path = require('path');
-const cors = require('cors');
-
+ 
 const app = express();
-
-//app.use(cors());
-
-// Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, 'dist/NewLook-T')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/NewLook-T/index.html'));
+ 
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/angularapp'));
+ 
+app.get('/*', function(req,res) {
+ 
+res.sendFile(path.join(__dirname+'/angularapp/index.html'));
 });
-
-const port = process.env.PORT || 8080;
-app.set('port', port);
-
-const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+ 
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
